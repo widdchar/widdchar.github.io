a236ead970b3cd5edb09561adfd985f657fc8336
widdchar <widdchar@gmail.com>
2021-05-13 19:11:38 -0600

    5/13

--- lesson4/preston-4.js
+++ lesson4/preston-4.js
@@ -1,3 +1,42 @@
+//Toggle Menu Section
+
 function toggleMenu() {
   document.getElementById("primaryNav").classList.toggle("hide"); 
 }
+
+//Date Section
+
+const days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+];
+
+const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+];
+
+const date = new Date();
+const dayOfMonth = date.getDate();
+const year = date.getFullYear();
+const day = days[date.getDay()];
+const month = months[date.getMonth()];
+const currentDate = '${day}, ${dayOfMonth} ${month} ${year}';
+
+document.querySelector('#currentYear').innerHTML = year;
+document.querySelector('#currentDate').innerHTML = currentDate;
