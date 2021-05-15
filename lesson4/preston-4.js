8ca6f50dfef285b0b538673921c3f073bf1aa767
widdchar <widdchar@gmail.com>
2021-05-14 22:54:24 -0600

    Nav fix

--- lesson4/preston-4.js
+++ lesson4/preston-4.js
@@ -1,8 +1,13 @@
 //Toggle Menu Section
 
-function toggleMenu() {
-  document.getElementById("primaryNav").classList.toggle("hide"); 
-}
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
 
 //Date Section
 
