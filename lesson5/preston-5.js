af7a577576463fed70a0d570ae1f6f17fabe100c
widdchar <widdchar@gmail.com>
2021-05-20 15:13:49 -0600

    javascript banner

--- lesson5/preston-5.js
+++ lesson5/preston-5.js
@@ -22,4 +22,7 @@ let thedate = new Date ();
 
     if (thedate.getDay() == 5) {
     document.querySelector('#banner').style.display = "block";
-};
+    }
+    else {
+       document.querySelector('#banner').style.display = 'none';
+    }
