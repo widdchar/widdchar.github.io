7ecac232dc99ea09e72eaef53e23073365ddb1c5
widdchar <widdchar@gmail.com>
2021-05-26 21:21:45 -0600

    setting up lesson 6

---
+++ lesson6/preston-6.js
@@ -0,0 +1,28 @@
+//Toggle Menu Section
+
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
+
+// derive the current date using a date object
+const now = new Date(); 
+
+const fulldate = new Intl.DateTimeFormat("en-UK", { dateStyle: "full" }).format(
+	now);
+
+document.getElementById("date").innerHTML=fulldate
+
+//Banner
+let thedate = new Date ();
+
+    if (thedate.getDay() == 5) {
+    document.querySelector('#banner').style.display = "block";
+    }
+    else {
+       document.querySelector('#banner').style.display = 'none';
+    }
