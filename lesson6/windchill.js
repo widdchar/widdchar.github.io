6fecf6422357867b13f9cb8a15477b848d51bfbe
widdchar <widdchar@gmail.com>
2021-05-28 20:22:33 -0600

    spellimng error fix

--- lesson6/windchill.js
+++ lesson6/windchill.js
@@ -3,12 +3,12 @@ const windSpeed = docment.querySelector(".wind-speed").innerHTML;
 const windChill = document.querySelector(".wind-chill");
 
 if ((temp <= 50) && (windSpeed >3)) {
-    windChillFactir(temp, windSpeed);
+    windChillFactor(temp, windSpeed);
 } else{
     windChill.textContent = "N/A";
 }
 
-function windChillFactir(temp, windSpeed) {
+function windChillFactor(temp, windSpeed) {
     const windChillFormula = 
         35.74 + 0.6215 * temp - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temp * Math.pow(windSpeed, 0.16);
     windChill.innerHTML = '${windChillFormula.toFixed(0)}&deg;F';
