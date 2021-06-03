18b0425e1670bfa7eedf9b75c759476958c448c6
widdchar <widdchar@gmail.com>
2021-06-02 20:20:38 -0600

    space removal

--- lesson7/lazyload.js
+++ lesson7/lazyload.js
@@ -2,7 +2,7 @@ let imagesToLoad = document.querySelectorAll("img[data-src]");
 
 const imgOptions = {
     threshold: 1,
-    rootMargin: "0px 0px 50px 0 px",
+    rootMargin: "0px 0px 50px 0px",
 };
 
 const loadImages = (Image) => {
