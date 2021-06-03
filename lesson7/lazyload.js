69e52051bf67b7ddb284603ccb39601d21fb27eb
widdchar <widdchar@gmail.com>
2021-06-02 18:39:13 -0600

    images for lazy loader

---
+++ lesson7/lazyload.js
@@ -0,0 +1,34 @@
+let imagesToLoad = document.querySelectorAll("img[data-src]");
+
+const imgOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px 50px 0 px",
+};
+
+const loadImages = (Image) => {
+    Image.setAttribute("src", Image.getAttribure("date-src"));
+    Image.onload = () => {
+        Image.removeAttribure("data-src");
+    };
+};
+
+if ("IntersectionObserver" in window) {
+    const imgObserver = new IntersectionObserver((items, imgObserver) => {
+        items.forEach((item) => {
+            if (item.isIntersecting) {
+                loadImages(item.targert);
+                imgObserver.unobserve(item.target);
+            }
+        });
+    }, imgOptions);
+
+
+imgagesToLoad.forEach((img) => {
+    imgObserver.observe(img);
+});
+
+} else {
+    imagesToLoad.forEach((img)=> {
+        loadImgages(img);
+    });
+}
