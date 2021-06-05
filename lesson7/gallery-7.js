3d4ac5ce6ea2dcbb72cef757ffd5bd4038b0f46a
widdchar <widdchar@gmail.com>
2021-06-05 16:55:26 -0600

    push

--- lesson7/gallery-7.js
+++ lesson7/gallery-7.js
@@ -4,3 +4,14 @@ const hambutton = document.querySelector('.ham');
 const mainnav = document.querySelector('.navigation')
 
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+let a = new Date();
+let b = new Date();
+localStorage.a = a;
+localStorage.b = b;
+a = Date.parse(localStorage.a);
+b = Date.parse(localStorage.b);
+let daysSinceLastVisit = b - a;
+console.log(daysSinceLastVisit);
+
+document.querySelector('#days').textContent = daysSinceLastVisit
