f919f5ee405a579017e82d62f22862b77ea03407
widdchar <widdchar@gmail.com>
2021-06-26 20:38:19 -0600

    more fixes

--- lesson10/script.js
+++ lesson10/script.js
@@ -58,3 +58,69 @@ stormOutput.textContent - stormSlider.value;
 stormSlider.oninput = () => {
     stormOutput.textContent = stormSlider.value;
 };
+
+//Weather API
+
+const fiveDayURL = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=999a399ab9c14e5416d862673ac547b0&units=imperial";
+
+fetch(prestonURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    const temp = jsObject.main.temp;
+    document.getElementById("current-temp").textContent = temp.toFixed(0);
+
+    const currentWeather = jsObject.weather[0].main;
+    document.getElementById("description").textContent = currentWeather;
+
+    const highTemp = jsObject.main.temp_max;
+    document.getElementById("high-temp").textContent = highTemp.toFixed(0);
+
+    const lowTemp = jsObject.main.temp_min;
+    document.getElementById("low-temp").textContent = lowTemp.toFixed(0);
+
+    const humidity = jsObject.main.humidity;
+    document.getElementById("humidity").textContent = humidity;
+
+    const windSpeed = jsObject.wind.speed;
+    document.querySelector(".wind-speed").textContent = windSpeed.toFixed(0);
+
+    windChillFactor(temp, windSpeed);
+  });
+
+// //////// Forecast API ////////////////
+const prestonForecastURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=836d037508b2397deaea6c8b8b8b90d9";
+
+fetch(prestonForecastURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    const dataLists = jsObject.list;
+
+    for (data of dataLists) {
+      if (data.dt_txt.includes("18:00:00")) {
+        // Create a forecast box(card) to contain 5day forecast information
+        const forecastBox = document.createElement("div");
+        forecastBox.classList.add("forecast-box");
+
+        const dayOfWeek = document.createElement("div");
+        dayOfWeek.classList.add("day");
+        const days = new Date(data.dt_txt);
+        dayOfWeek.textContent = days.toString().slice(0, 3);
+        forecastBox.appendChild(dayOfWeek);
+
+        const image = document.createElement("img");
+        image.setAttribute(
+          "src",
+          "https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png"
+        );
+        image.setAttribute("alt", data.weather[0].description);
+        forecastBox.appendChild(image);
+
+        const forecastTemp = document.createElement("div");
+        forecastTemp.innerHTML = data.main.temp.toFixed(0) + "&deg;F";
+        forecastBox.appendChild(forecastTemp);
+
+        const forecast = document.querySelector(".forecast-container");
+        forecast.appendChild(forecastBox);
+      }
+    }
+  });
--- lesson10/weatherapi.js
+++ lesson10/weatherapi.js
@@ -4,15 +4,55 @@ const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Frida
 
 fetch(apiURL)
   .then((response) => response.json())
-  .then((jsObject) => {
-    console.log(jsObject);
-
-    document.getElementById('current-temp').textContent = jsObject.list[0].main.temp;
-
-    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.list[0].weather[0].icon + '.png';  // note the concatenation
-    const desc = jsObject.list[0].weather[0].description;  // note how we reference the weather array
-    document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
-    document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
-    document.getElementById('icon').setAttribute('alt', desc);
+  .then((forecast) => {
+    console.log(forecast);
+    document.getElementById("current-temp").textContent = Math.round(
+      forecast.list[0].main.temp
+    );
+    // Iterate over the object list
+    for (let x in forecast.list) {
+      // Check if the object is the data taken at 6pm
+      if (forecast.list[x].dt_txt.includes("18:00:00")) {
+        // Create the parent container, "day-box"
+        let dayBox = document.createElement("div");
+        dayBox.setAttribute("class", "day-box");
+        // Get the day of the week and append that first
+        let weekDay = document.createElement('div');
+        weekDay.setAttribute('class', 'week-day');
+        let dayOfWeek = new Date(forecast.list[x].dt_txt);
+        let dayNum = dayOfWeek.getDay();
+        weekDay.textContent = weekdays[dayNum];
+        dayBox.appendChild(weekDay);
+        // Create a child element to hold the temperature for the day
+        let foreTemp = document.createElement("p");
+        foreTemp.setAttribute("class", "fore-temp");
+        foreTemp.innerHTML = Math.round(forecast.list[x].main.temp_max) + " &deg;F";
+        dayBox.appendChild(foreTemp);
+        // Create a child element to get the icon for the forecasted weather
+        let weatherIcon = document.createElement('img');
+        const imagesrc = 'https://openweathermap.org/img/w/' + forecast.list[x].weather[0].icon + '.png';
+        const desc = forecast.list[x].weather.description;
+        weatherIcon.setAttribute('class', 'weather-icon');
+        weatherIcon.setAttribute('src', imagesrc);
+        weatherIcon.setAttribute('alt', desc);
+        dayBox.appendChild(weatherIcon);
+        // Append the "day-box" to the parent section in the HTML
+        document.querySelector(".forecast-test").appendChild(dayBox);
+      }
+    }
   });
+
+//fetch(apiURL)
+//  .then((response) => response.json())
+//  .then((jsObject) => {
+//    console.log(jsObject);
+
+//    document.getElementById('current-temp').textContent = jsObject.list[0].main.temp;
+
+//    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.list[0].weather[0].icon + '.png';  // note the concatenation
+//    const desc = jsObject.list[0].weather[0].description;  // note how we reference the weather array
+//    document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+//    document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+//    document.getElementById('icon').setAttribute('alt', desc);
+//  });
 
