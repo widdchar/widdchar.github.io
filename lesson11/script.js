70c324f1fecc0e7419d0897044373dd336f7265e
widdchar <widdchar@gmail.com>
2021-07-05 20:14:01 -0600

    push

--- lesson11/script.js
+++ lesson11/script.js
@@ -215,3 +215,15 @@ fetch(eventURL)
       }
     }
   });
+
+  //*Days Since Last visit*/
+let a = new Date();
+let b = new Date();
+localStorage.a = a;
+localStorage.b = b;
+a = Date.parse(localStorage.a);
+b = Date.parse(localStorage.b);
+let daysSinceLastVisit = b - a;
+console.log(daysSinceLastVisit);
+
+document.querySelector('#days').textContent = daysSinceLastVisit
