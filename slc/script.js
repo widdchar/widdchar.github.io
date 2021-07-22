95d82db8c6abca2c0073c770ba01a593dbcd3f3a
widdchar <widdchar@gmail.com>
2021-07-21 21:37:47 -0600

    push

--- slc/script.js
+++ slc/script.js
@@ -4,3 +4,11 @@ const hambutton = document.querySelector('.ham');
 const mainnav = document.querySelector('.navigation')
 
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+//Last Modified Date
+const lastMod = document.lastModified;
+document.querySelector("#currentDate").textContent = lastMod;
+
+const date = new Date();
+const currentYear = date.getFullYear();
+document.querySelector("#currentYear").textContent = currentYear;
